f6f760d1ecff120a355bdf903a817e40c4e4ce43
fera0013 <ralphfehrer@gmail.com>
2018-02-08 13:29:26 +0100

    build_part1_RNN implemented

--- my_answers.py
+++ my_answers.py
@@ -3,6 +3,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Activation
 import keras
 
 
@@ -16,7 +17,11 @@ def window_transform_series(series, window_size):
 
 ## TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape = (window_size,1)))
+    model.add(Dense(1))
+    model.add(Activation('relu'))
+    return model
 
 
 #### TODO: return the text input with only ascii lowercase and the punctuation given below included.
