719758d4f25213a14be553cc756113edefcec937
fera0013 <ralphfehrer@gmail.com>
2018-02-11 19:34:32 +0100

    yet another whitespace strategy

--- my_answers.py
+++ my_answers.py
@@ -25,7 +25,7 @@ def build_part1_RNN(window_size):
 
 #### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
-    text = re.sub(r"[^A-Za-z!.:;,?\s]", "", text)
+    text = re.sub(r"[^A-Za-z!.:;,? ]", "", text)
     text.lower()
     return text
 
